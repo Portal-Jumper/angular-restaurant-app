165b627ec1047823174793ef54533f77770ab5fb
PortalJumper <hubertszymusiuk@gmail.com>
2022-01-12 11:13:55 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PackageForPizzalistCartComponent} from "./components/package-for-pizzalist-cart/package-for-pizzalist-cart.component";
+import {CartComponent} from "./components/cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PackageForPizzalistCartComponent},
+  {path: 'login', component: CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
